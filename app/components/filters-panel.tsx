d5e7bad225a1d4a72aba30ea28e00d482d91d85e
RemyVK <remykaredan@gmail.com>
2026-09-04 21:24:02 +0200

    Implemented the skelton

---
+++ app/components/filters-panel.tsx
@@ -0,0 +1,5 @@
+export default function FiltersPanel() {
+    return (
+        <>filters goes here</>
+    )
+}
--- app/pages/home-page.tsx
+++ app/pages/home-page.tsx
@@ -1,5 +1,16 @@
+import FiltersPanel from '~/components/filters-panel';
+import './home-page.css';
+import OfferCrad from '~/components/offer-card';
+
 export default function HomePage() {
     return (
-        <>Hey</>
+        <>
+            <header><h1>InsureCompare</h1></header>
+            <div className='content'>
+                <FiltersPanel />
+                <OfferCrad />
+            </div>
+        </>
+
     )
 }
